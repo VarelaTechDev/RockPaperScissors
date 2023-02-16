388f3b1217a74919ecea1157fa1515943378acd9
ArisaBonsaiTree <angelevarela@hotmail.com>
2023-02-15 16:19:19 -0800

    Settings > Editor > Inlay Hints. Also implemented most of the logic

--- src/main/java/com/game/Main.java
+++ src/main/java/com/game/Main.java
@@ -1,71 +1,109 @@
 package com.game;
 
-
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     private int numberOfTies, computerWins, humanWins, numberOfRounds;
-    private Scanner scanner;
-    private HashMap<Integer, Integer> victoryMap;
-    private Random random;
+    private Scanner scanner = new Scanner(System.in);
+    private Random random = new Random();
+    private HashMap<Integer, Integer> victoryMap = new HashMap();
+    private final String[] choices = {"Rock", "Paper", "Scissor"};
+
+    private final String humanName = "Human";
+    private final String computerName = "CPU";
+    private final int minRound = 1;
+    private final int maxRound = 10;
 
     public static void main(String[] args) {
         new Main().driver();
     }
 
-    private void driver(){
-        scanner = new Scanner(System.in);
-        random = new Random();
-
-        // We should empty out any values we have for ties, wins, and lose
+    private void driver() {
+        setVictoryConditions();
         setVariablesToZero();
-
-        // Next get the amount of rounds we want
         setNumberOfRounds();
 
-        System.out.printf("Cool, we are going to play %d rounds", numberOfRounds);
-
-        // ToDo: Need to implement the loop
+        playGame(numberOfRounds);
+        printResult();
     }
 
-    private void setNumberOfRounds(){
-        System.out.println("Between 1 - 10, how many rounds would you like to play?");
-        try {
-            numberOfRounds = scanner.nextInt();
-
-            if(numberOfRounds < 1 || numberOfRounds > 10) throw new IndexOutOfBoundsException();
-
-        }
-        // Prevent OutOfBounds and if the user types a non-integer value WE EXIT THE PROGRAM
-        catch (Exception e){
-            // Go here if we enter something invalid
-            System.out.println("Invalid input");
-            System.exit(0);
-        }
-    }
-
-
-    // If playerOne maps to the value, we know playerOne choice beat playerTwo
-    private boolean doesPlayerOneWin(int playerOne, int playerTwo){
-        return victoryMap.get(playerOne) == playerTwo;
-    }
-
-    // By using a HashMap, we can easily change the code logic
-    // Rock(0) BEATS Scissors(2)
-    // Paper(1) BEATS Rock(0)
-    // Scissor(2) BEATS Paper(1)
-    private void setVictoryConditions(){
-        victoryMap = new HashMap();
+    private void setVictoryConditions() {
         victoryMap.put(0, 2);
         victoryMap.put(1, 0);
         victoryMap.put(2, 1);
     }
 
-    private void setVariablesToZero(){
+    private void setVariablesToZero() {
         numberOfTies = 0;
         computerWins = 0;
         humanWins = 0;
+    }
+
+    private void setNumberOfRounds() {
+        System.out.printf("Between %d - %d, how many rounds would you like to play? ", minRound, maxRound);
+        try {
+            numberOfRounds = scanner.nextInt();
+
+            if (numberOfRounds < minRound || numberOfRounds > maxRound) throw new IndexOutOfBoundsException();
+
+        }
+        // Prevent OutOfBounds and if the user types a non-integer value WE EXIT THE PROGRAM
+        catch (Exception e) {
+            // Go here if we enter something invalid
+            System.out.printf("Error: Invalid input! Only NATURAL NUMBERS from %d - %d. Closing the program!\n", minRound, maxRound);
+            System.exit(0);
+        }
+    }
+    private void playGame(int numberOfRounds){
+        int userChoice, computerChoice;
+
+        for(int i = 1; i <= numberOfRounds; i++){
+            printChoices();
+
+            userChoice = scanner.nextInt();
+            computerChoice = random.nextInt(3);
+
+            System.out.printf("Round: %d\t", i);
+            System.out.printf("%s: %s\t%s: %s\n", humanName, choices[userChoice], computerName, choices[computerChoice]);
+
+            determineWinner(userChoice, computerChoice);
+        }
+    }
+    private void printResult(){
+        System.out.println("=========================");
+
+        System.out.printf("Number of ties: %d\nNumber of %s wins: %d\nNumber of %s wins: %d\n", numberOfTies, humanName, humanWins, computerName, computerWins);
+
+        if(humanWins > computerWins) System.out.printf("Winner: %s\n", humanName);
+        else if(computerWins > humanWins) System.out.printf("Winner: %s\n", computerName);
+        else System.out.println("Tie");
+
+        System.out.println("=========================");
+    }
+
+    private void printChoices(){
+        System.out.printf("Type a number corresponding with your choice: ", choices.length);
+        for(int i = 0; i < choices.length; i++){
+            System.out.printf("%d = %s\t", i + 1, choices[i]);
+        }
+        System.out.printf(": ");
+    }
+
+    private void determineWinner(int userChoice, int computerChoice){
+        if(userChoice == computerChoice){
+            System.out.println("Tie");
+            numberOfTies++;
+        }
+        else if(victoryMap.get(userChoice) == computerChoice){
+            System.out.printf("%s won\n", humanName);
+            humanWins++;
+        }
+        else{
+            System.out.printf("%s won\n", computerName);
+            computerWins++;
+        }
+        System.out.println("==========");
     }
 }
